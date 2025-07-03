9b7b584307865f92427098bb06c5f9f0445c3ae0
박종혁 <jonghyeok@jobis.co>
2025-07-03 22:18:42 +0900

    feat: accordion

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,12 @@ import { useState } from 'react'
 
 import { ShareCopy } from '@/components/share-copy'
 import { ShareKakao } from '@/components/share-kakao'
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from '@/components/ui/accordion'
 import { Input } from '@/components/ui/input'
 import Segmented from '@/components/ui/segmented'
 
@@ -35,6 +41,33 @@ const HomePage = () => {
         buttonTitle="Google"
       />
       <ShareCopy url="https://www.google.com" />
+
+      <Accordion type="single" collapsible>
+        <AccordionItem value="1">
+          <AccordionTrigger>Lorem ipsum dolor sit 1</AccordionTrigger>
+          <AccordionContent>
+            adipisicing elit. Harum esse minus assumenda maiores earum? Sequi
+            doloribus omnis ratione error, at, commodi laboriosam ullam possimus
+            minus ut saepe! Ad, corrupti dolor!
+          </AccordionContent>
+        </AccordionItem>
+        <AccordionItem value="2">
+          <AccordionTrigger>Lorem ipsum dolor sit 2</AccordionTrigger>
+          <AccordionContent>
+            adipisicing elit. Harum esse minus assumenda maiores earum? Sequi
+            doloribus omnis ratione error, at, commodi laboriosam ullam possimus
+            minus ut saepe! Ad, corrupti dolor!
+          </AccordionContent>
+        </AccordionItem>
+        <AccordionItem value="3">
+          <AccordionTrigger>Lorem ipsum dolor sit 3</AccordionTrigger>
+          <AccordionContent>
+            adipisicing elit. Harum esse minus assumenda maiores earum? Sequi
+            doloribus omnis ratione error, at, commodi laboriosam ullam possimus
+            minus ut saepe! Ad, corrupti dolor!
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
     </div>
   )
 }
---
+++ src/components/ui/accordion.tsx
@@ -0,0 +1,71 @@
+'use client'
+
+import * as AccordionPrimitive from '@radix-ui/react-accordion'
+import { ChevronDown } from 'lucide-react'
+import * as React from 'react'
+
+import { cn } from '@/lib/utils'
+
+function Accordion({
+  ...props
+}: React.ComponentProps<typeof AccordionPrimitive.Root>) {
+  return <AccordionPrimitive.Root data-slot="accordion" {...props} />
+}
+
+function AccordionItem({
+  className,
+  ...props
+}: React.ComponentProps<typeof AccordionPrimitive.Item>) {
+  return (
+    <AccordionPrimitive.Item
+      data-slot="accordion-item"
+      className={cn(
+        'border-border nb-shadow overflow-hidden rounded',
+        className
+      )}
+      {...props}
+    />
+  )
+}
+
+function AccordionTrigger({
+  className,
+  children,
+  ...props
+}: React.ComponentProps<typeof AccordionPrimitive.Trigger>) {
+  return (
+    <AccordionPrimitive.Header className="flex">
+      <AccordionPrimitive.Trigger
+        data-slot="accordion-trigger"
+        className={cn(
+          'bg-primary flex flex-1 items-center justify-between p-3 text-left text-sm transition-all focus-visible:ring-[3px] disabled:pointer-events-none disabled:opacity-50 data-[state=open]:rounded-b-none data-[state=open]:border-b [&[data-state=open]>svg]:rotate-180',
+          className
+        )}
+        {...props}
+      >
+        {children}
+        <ChevronDown className="pointer-events-none size-5 shrink-0 transition-transform duration-200" />
+      </AccordionPrimitive.Trigger>
+    </AccordionPrimitive.Header>
+  )
+}
+
+function AccordionContent({
+  className,
+  children,
+  ...props
+}: React.ComponentProps<typeof AccordionPrimitive.Content>) {
+  return (
+    <AccordionPrimitive.Content
+      data-slot="accordion-content"
+      className="data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down overflow-hidden bg-white text-sm transition-all"
+      {...props}
+    >
+      <div className={cn('p-3', className)}>{children}</div>
+    </AccordionPrimitive.Content>
+  )
+}
+
+AccordionContent.displayName = AccordionPrimitive.Content.displayName
+
+export { Accordion, AccordionContent, AccordionItem, AccordionTrigger }
